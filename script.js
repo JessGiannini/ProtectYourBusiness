98622986d14009421efd5820577ca269cc5edb2d
JessGiannini <jessgiannini@gmail.com>
2021-06-22 17:54:48 -0700

    work with tutor basic outline

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var number = ["0", "1", "2", "3"..."9"]
+var lowerCaseLetter
+var upperCaseLetter
+var specialCharacter
+var random = function(x){
+  var value = x[Math.floor(Math.random() * x.length)]
+  return value
+}
+// while statements
+function generatePassword() {
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +25,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
