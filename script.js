f6308fc783e124b004b94658e904ce0f93131bf2
JessGiannini <jessgiannini@gmail.com>
2021-06-24 09:52:35 -0700

    updating main function

--- script.js
+++ script.js
@@ -2,33 +2,51 @@
 var generateBtn = document.querySelector("#generate");
 
 var validLength = [];
-var validInput = Range(8, 128)
+//var validInput = Range(8, 128)
 
-var userLength = prompt("Enter the length of password between 8 - 128.");
 passwordLength()
 
 function passwordLength() {
-userLength = prompt("Enter the length of password between 8 - 128.");
-if (userLength<8||userLength>128||userLength==isNaN(userLength)===true)
-  var userLength
-else if(userLength === null)
-  alert("Please enter a number between 8 - 128.")
-else
-    askAgain()
+ var userLength = prompt("Enter the length of password between 8 - 128.")
+ console.log(userLength)
+if (userLength>8 && userLength<128) {
+  var userNumber = confirm("Do you want to include numbers?");
+  var userSpecial = confirm("Do you want to include special characters?");
+  var userAlphaUpper = confirm("Do you want to include upper case letters?");
+  var userAlphaLower = confirm("Do you want to include lower case letters?");
+if (userNumber === true || userSpecial === true || userAlphaUpper === true || userAlphaLower === true) {
+  console.log("checking user entry") 
+} else {
+  alert("Please make at least one selection!")
 }
 
-function askAgain(){
-  userLength = prompt("Enter the length of password between 8 - 128.");
-if (userLength<8||userLength>128||userLength==isNaN(userLength)===true)
-  var userLength
-else if(userLength === null)
-  alert("Please enter a number between 8 - 128.")
-else
-    askAgain()
+}
+else if(userLength === null){
+console.log("i am a null")  
+alert("Please enter a number between 8 - 128.")
+} else {
+    console.log("this is the else")
+  passwordLength()
+}
 }
 
+// function askAgain(){
+//   var userLength = prompt("Enter the length of password between 8 - 128.")
+//  console.log(userLength)
+// if (userLength>8 && userLength<128) {
+// console.log("i am here")}
+// else if(userLength === null){
+// console.log("i am a null")  
+// alert("Please enter a number between 8 - 128.")
+// } else {
+//     console.log("this is the else")
+//   askAgain()
+// }
+// }
 
 
+
+ //functions used to create password characters
 function lowerAlpha() {
   return String.fromCharCode(Math.floor(Math.random() *26) +97) 
 }
@@ -48,27 +66,28 @@ function RandomSpecial() {
 
 
 
-//for (let i = 0; i < userLength; i++){
-  
-
+// for (let i = 0; i < userLength; i++){
+//   while (userAlphaUpper === true) {
+//     upperAlpha()
+//   }
+// }
 
 
 
 // while statements
- function generatePassword() {
-
- }
+//  function generatePassword() {
+  
+//  }
 
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
+//generateBtn.addEventListener("click", writePassword){}}
