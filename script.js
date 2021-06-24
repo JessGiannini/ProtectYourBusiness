611bf6a5fdda2624e9a5b24808e93d972a5c3017
JessGiannini <jessgiannini@gmail.com>
2021-06-23 21:59:10 -0700

    added password criteria

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var number = ["0", "1", "2", "3"..."9"]
-var lowerCaseLetter
-var upperCaseLetter
-var specialCharacter
-var random = function(x){
-  var value = x[Math.floor(Math.random() * x.length)]
-  return value
+
+function lowerAlpha() {
+  return String.fromCharCode(Math.floor(Math.random() *26) +97) 
 }
+
+function upperAlpha() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) +65)
+}
+
+function RandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() *10) +48);
+}
+
+function RandomSpecial() {
+  const specialCharacter = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+  return specialCharacter[Math.floor(Math.random() * specialCharacter.length)];
+}
+
+// var random = function(x){
+//   var value = x[Math.floor(Math.random() * x.length)]
+//   return value
+// }
+
+var userLength = prompt("Select length of password between 8-128.");
+var userNumber = confirm("Do you want to include numbers?");
+var userSpecial = confirm("Do you want to include special characters?");
+var userAlpha = confirm("Do you want to include letters?");
+
+
+
 // while statements
 function generatePassword() {
 
+  if (userLength === alphabet) {
+array.push(alphabet)
+  }
+
 }
+
 
 // Write password to the #password input
 function writePassword() {
